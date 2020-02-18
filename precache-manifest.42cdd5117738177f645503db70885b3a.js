e29759a2fecc138246d0629fe8f1338a9dc840a1
Mariusz <support@tytax.com>
2020-02-18 10:49:52 +0100

    Updates

---
+++ precache-manifest.42cdd5117738177f645503db70885b3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1069241f9e4c3e5a564",
+    "url": "/app/static/css/main.0e3b72e7.chunk.css"
+  },
+  {
+    "revision": "b1069241f9e4c3e5a564",
+    "url": "/app/static/js/main.bd5218b4.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "a3fe8f4ea585841627a9",
+    "url": "/app/static/js/2.c3db515b.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "d0919bd2b6567dcb93487e39d73e2734",
+    "url": "/app/index.html"
+  }
+];
